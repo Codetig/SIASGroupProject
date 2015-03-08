5be0e423b0d274f06db89a0aefa598473db3060a
Ifeoluwa Omoniyi <codetig@outlook.com>
2015-03-08 15:05:39 -0700

    Track-Create action works-can be refactored later

--- app/assets/javascripts/tracks.js
+++ app/assets/javascripts/tracks.js
@@ -1,39 +1,75 @@
 //=======  Get tracks search result =========//
 
 $(document).ready(function () {
- $("form").on("submit", function (event) {
-   event.preventDefault();
 
+//variable to store spotify result and track titles
+  var spResult ={};
+
+ $("#search form").on("submit", function (event) {
+   event.preventDefault();
+   $('#results ul').empty();  //this clears out old search results
    var query = this.searchTerm.value;
    $.getJSON("https://api.spotify.com/v1/search?q=" + query + "&type=track,artist&market=US", function(data) {
-     var $results = $("#results");   
+     var resultList = $("#results ul");  // changed this variable name from $results to resultList to avoid confusion 
      for (var i = 0; i < data.tracks.items.length; i++) {
         var title = data.tracks.items[i].name;
         var artist = data.tracks.items[i].artists[0].name;
         var preview = data.tracks.items[i].preview_url;
+        spResult[title+artist] = data.tracks.items[i];
         
-        $results.append("<li><strong>"+title + "</strong> - " + artist + " <a  href="+ preview + ">Preview</a>&nbsp;<a href='' id='create_track'  remote = 'true'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></li><br>");
-  
-        $('li').click('#create_track',function (event) { 
-          event.preventDefault(); 
-          var formURL = $("form").attr("action");
-          var postData = data.tracks;
-          $.ajax({
-            url : formURL,
-            type: "POST",
-            data : postData,
-            success:function(data, textStatus, jqXHR) 
-            {
-                //data: return data from server
-                // data
-            }
-             });
-          $(this).fadeOut();    
-        });
-        // $results = $results.append("<iframe src=\"https://embed.spotify.com/?uri=" + data.tracks.items[i].uri+"\" width=\"300\" height=\"380\" frameborder=\"0\" allowtransparency=\"true\"></iframe>");
+        resultList.append("<li><span class='tr-title'><b>" + title + "</b></span> - " + "<span class='tr-artist'>" + artist + "</span> <a href="+ preview + ">Preview</a>  <a href='' class='create_track'  remote = 'true'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></a></li>");
+        //removed on click listener on <li>s from here -Ife
+        // resultList = resultList.append("<iframe src=\"https://embed.spotify.com/?uri=" + data.tracks.items[i].uri+"\" width=\"300\" height=\"380\" frameborder=\"0\" allowtransparency=\"true\"></iframe>");
       }
+  }); //end of getJSON
+ }); //end of searh form submit
+
+  //Ife's changed listener
+  $('#results ul').on('click', '.create_track',function (e) { 
+    e.preventDefault(); 
+    var formURL = $("#search form").attr("action");
+    console.log($(this).parent().find($('.tr-title')).text());
+    var title = $(this).parent().find($('.tr-title')).text();
+    var artist = $(this).parent().find($('.tr-artist')).text();
+  
+    var postData = spResult[title+artist];
+    console.log(postData);
+    console.log(harry);
+    $.ajax({
+      url : formURL,
+      type: "POST",
+      data : {track: {spotify_track_id: postData.id, title: postData.name, track_uri: postData.uri, artist: postData.artists[0].name}},
+      success:function(data, textStatus, jqXHR) 
+      {
+        location.reload();
+        console.log("It worked!");
+          //data: return data from server
+          // data
+      }
+       });
+    $('#results ul').empty();    
   });
- });
+
+  //Sue's original list listener
+  // $('li').click('#create_track',function (event) { 
+  //   event.preventDefault(); 
+  //   var formURL = $("form").attr("action");
+  //   //var index = spResult.indexOf($('#tr-title').text());
+  //   var postData = spResult[$('#tr-title').text()];
+  //   console.log(postData);
+  //   $.ajax({
+  //     url : formURL,
+  //     type: "POST",
+  //     data : postData,
+  //     success:function(data, textStatus, jqXHR) 
+  //     {
+  //         //data: return data from server
+  //         // data
+  //     }
+  //      });
+  //   $(this).fadeOut();    
+  // });
+
 });
 
 
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,17 +1,30 @@
 class TracksController < ApplicationController
+  
+
   def create
-    @track = Track.new(params[:track])
-    binding.pry
-    respond_to do |format|
-      if @track.save
-        format.html { redirect_to tracks_url }
-        format.json { head :ok }
-      else
-        format.html
-        format.json
-      end
-      format.js
+    #tr_info = {}
+    @track = Track.new(track_params) #ife-changed this from prams[:tracks]
+    @bucket = Bucket.find(params[:bucket_id])
+    @track.buckets << @bucket  #adding track to associated bucket
+    # binding.pry
+    if @track.save
+      redirect_to @bucket, notice: "#{@track.title} has been added"
+      # binding.pry
+    else
+      redirect_to @bucket, notice: "unable to add #{@track.title}"
     end
+
+    # commented out the code below because I am not sure of its function
+    # respond_to do |format|
+    #   if @track.save
+    #     format.html { redirect_to tracks_url }
+    #     format.json { head :ok }
+    #   else
+    #     format.html
+    #     format.json
+    #   end
+    #   format.js
+    # end
   end
 
   def update
@@ -33,6 +46,6 @@ class TracksController < ApplicationController
 
   private 
   def track_params
-    params.require(:track).permit(:title, :uri, :artist, :spotify_track_id, :play_count)
+    params.require(:track).permit(:title, :track_uri, :artist, :spotify_track_id) #removed playcount, we don't want user adding it
   end
 end
