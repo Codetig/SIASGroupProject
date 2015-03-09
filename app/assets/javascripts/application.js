e51f644761b83a4e56aafcc7563cdcd4435b8fa0
Ifeoluwa Omoniyi <codetig@outlook.com>
2015-03-08 22:22:48 -0700

    Added edit form for&to buckets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 
 $(document).ready(function(){
   $('#add-form').fadeOut(0);
-  $('#edit-form').fadeOut(0);
+  $('.edit-form').fadeOut(0);
   $('.add-link').on('click', function(e){
     e.preventDefault();
     $('#edit-form').fadeOut(0);
@@ -25,10 +25,14 @@ $(document).ready(function(){
     // $('#add-form').append('<h2>Add New Bucket</h2><form action="/users/:user_id/buckets"><label for="name">Name: </label><input type="text" name="name" id="name"><br><label for="desc">Description: </label><input type="text" name="desc" id="desc"><input type="submit" value="Add Bucket"></form>');
   }); //end of add-link click
 
-  $('.edit-link').on('click', function(e){
+  $('.edit').on('click', function(e){
     e.preventDefault();
     $('#add-form').fadeOut(0);
-    $('#edit-form').fadeIn(0);
+    $('.edit-form').fadeOut(0);
+
+    // console.log($(this).parent().parent().find('form')); //testing selection
+    var editForm = $(this).parent().parent().find('.edit-form');
+    editForm.fadeIn(0);
   }); //end of add-link click
 
 });
