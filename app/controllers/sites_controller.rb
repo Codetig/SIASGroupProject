2a0b895b21e73bd704041098e9070281e0773a35
Shilpa Kumar <skumar225@gmail.com>
2015-03-12 17:58:12 -0700

    EVERYTHING IS WORKING?

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -12,7 +12,6 @@ class SitesController < ApplicationController
 
   def guest_login
     @user = User.find_by(name: "Guest User") || User.create(name: "Guest User", first_name: "Guest", last_name: "User", uid: 111222333, email: "visitor@site.com")
-    binding.pry
     session[:user_id] = @user.id
     redirect_to user_buckets_path(@user)
   end
